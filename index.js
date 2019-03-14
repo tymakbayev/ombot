b96d07d83dfb0c344da568d184eb94744711b7ad
tymakbayev <tymakbayevabai@gmail.com>
2019-03-15 03:41:02 +0600

    initial

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const TOKEN = '777413269:AAFUzxudWEyUs_nmUxw4tNPipVSqYQ8BlO0'
 
 const bot = new TelegramBot(TOKEN,{polling: true})
 
-bot.on('message', msg => {
-    bot.sendMessage(msq.chat.id, 'Hello from Abay, bot says: "Hi, ${msg.from.first_name}"')
-})
+bot.onText(//echo (.+)/, function (msg, match) {
+var fromId = msg.from.id;
+var resp = match[1];
+bot.sendMessage(fromId, resp);
+});
+bot.on('message', function (msg) {
+    var chatId = msg.chat.id;
+    // Фотография может быть: путь к файлу, поток(stream) или параметр file_id
+    var photo = 'cats.png';
+    bot.sendPhoto(chatId, photo, {caption: 'Милые котята'});
+});
