5f2a7831e2435329f55e987aca7b220b5611f39f
tymakbayev <tymakbayevabai@gmail.com>
2019-03-15 03:58:05 +0600

    initial

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const TelegramBot = require('node-telegram-bot-api')
 
+require('http').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
+    res.end('')
+})
 const TOKEN = '777413269:AAFUzxudWEyUs_nmUxw4tNPipVSqYQ8BlO0'
 
 const bot = new TelegramBot(TOKEN,{polling: true})
 
 bot.on('message', msg => {
-    bot.sendMessage(msq.chat.id, 'Hello from Abay, bot says: "Hi, ${msg.from.first_name}"')
+    bot.sendMessage(msq.chat.id, 'Hello from EVOTECH, bot says: "Hi, ${msg.from.first_name}"')
 })
